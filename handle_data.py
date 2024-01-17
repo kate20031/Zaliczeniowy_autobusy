2a28f54d100b314e379f900a0edfc0b521b8cb80
kate20031 <83357098+kate20031@users.noreply.github.com>
2024-01-17 21:45:10 +0100

    Add functionality to calculate average speed

--- handle_data.py
+++ handle_data.py
@@ -21,6 +21,27 @@ def convert_to_dict(str_dict):
     return eval(str_dict)
 
 
+def calculate_average_speed(group):
+    total_distance = 0
+    total_time = 0
+    sum_speed = 0
+
+    for i in range(1, len(group)):
+        time_diff = (datetime.strptime(group[i]['Time'], '%Y-%m-%d %H:%M:%S') -
+                     datetime.strptime(group[i - 1]['Time'], '%Y-%m-%d %H:%M:%S')).total_seconds()
+
+        lat1, lon1 = group[i - 1]['Lat'], group[i - 1]['Lon']
+        lat2, lon2 = group[i]['Lat'], group[i]['Lon']
+        distance = haversine_distance(lat1, lon1, lat2, lon2)
+        total_distance += distance
+        total_time += time_diff
+
+    average_speed = total_distance / total_time * 3.6 if total_time > 0 else 0
+    print(average_speed)
+
+    return average_speed
+
+
 csv_file_path = "output.csv"
 json_data = []
 
@@ -40,3 +61,6 @@ with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
 sorted_json_data = sorted(json_data, key=lambda x: (x['VehicleNumber'], x['Time']))
 grouped_json_data = {key: list(group) for key, group in groupby(sorted_json_data, key=lambda x: x['VehicleNumber'])}
 
+for key, group in grouped_json_data.items():
+    avg_speed = calculate_average_speed(group)
+    print(f"VehicleNumber {key}: Average Speed = {avg_speed:.2f} meters per second")
