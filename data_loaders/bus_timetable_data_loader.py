5d4ee975071690cf9876ba28f1c2f09de36b7ee9
kate20031 <83357098+kate20031@users.noreply.github.com>
2024-02-08 02:25:12 +0100

    Remove data_loader.py and add requirements.txt

---
+++ data_loaders/bus_timetable_data_loader.py
@@ -0,0 +1,24 @@
+"""
+This module loads the bus timetable data.
+"""
+
+from config.constants import API_KEY
+from utils import conect_to_api
+
+
+def load_bus_timetable_data(bus_stop_id, bus_stop_nr, line):
+    """
+    Fetches data from an API and saves it into a csv file.
+    """
+
+    id_1 = "e923fa0e-d96c-43f9-ae6e-60518c9f3238"
+
+    query_params = {
+        "id": id_1,
+        "apikey": API_KEY,
+        "busstopId": bus_stop_id,
+        "busstopNr": str(bus_stop_nr),
+        "line": line,
+    }
+
+    return conect_to_api(query_params)
